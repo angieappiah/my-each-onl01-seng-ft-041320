5dce1fa4b9af6276d41ed6964dec602e1b6b44f3
Angela Appiah <alegnalawn2@yahoo.com>
2020-04-15 23:58:37 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (numbers)
+  numbers = {"1", "2", "3", "4", "5","6"}
+my_each(numbers) 
+do |i|
+  puts i
+  my_each(numbers).return
+end
 end
