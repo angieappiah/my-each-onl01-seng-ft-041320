6c8e71efafcb0e133379c65e55cc85a581585728
Angela Appiah <alegnalawn2@yahoo.com>
2020-04-16 00:39:36 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,10 +4,10 @@ end
 
 list = ["dress", "bag", "shoe"]
 
-def my_each(array)
+def my_each(numbers)
   i = 0
-  while i < array.length
-    yield(array[i])
+  while i < numbers.length
+    yield(numbers[i])
     i += 1
       my_each(numbers).return
   end
