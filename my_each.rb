67d024e00f55644002082a0db6b477e03be81ba7
Angela Appiah <alegnalawn2@yahoo.com>
2020-04-16 00:28:23 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -9,3 +9,13 @@ def my_each(array)
     i = i + 1
   end
 end
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
